ad06b7d3f454d107d96d6368644fc47ed3049ec7
meenakshi452 <bethim21@iitk.ac.in>
2024-06-28 15:36:00 +0530

    create test api  route

--- mern-auth/api/index.js
+++ mern-auth/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,6 +12,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-app.listen(5173, ()=>{
+app.listen(3000, ()=>{
     console.log('server listeninggg');
 });
+
+app.use('/api/user', userRoutes);
